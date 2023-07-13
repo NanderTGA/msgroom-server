a10690e4f2e493f265079d74ca6d36d150ef91d0
its-pablo <itspablo@null.net>
2023-07-13 12:44:16 -0700

    add crypto-js, types, and add decrypt function

--- src/utils/generateID.ts
+++ src/utils/generateID.ts
@@ -7,5 +7,11 @@ import CryptoJS from "crypto-js"
  * @returns The generated ID
  */
 export default async function magic(socket: Socket): Promise<string> {
-    CryptoJS.AES.encrypt
+    if (!process.env.PASSWORD) throw new Error("No password provided")
+    const cipher = CryptoJS.AES.encrypt(socket.conn.remoteAddress, process.env.PASSWORD)
+    return cipher.toString()
+}
+export async function decrypt(cipher: CryptoJS.lib.CipherParams){
+    if (!process.env.PASSWORD) throw new Error("No password provided")
+    return CryptoJS.AES.decrypt(cipher, process.env.PASSWORD).toString(CryptoJS.enc.Utf8)
 }
