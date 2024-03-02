44c3b6938cfc6d7fad079e575ad7274335c93c42
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2024-03-02 12:51:49 +0100

    feat: proper IDs

--- src/utils/generateID.ts
+++ src/utils/generateID.ts
@@ -1,17 +1,5 @@
-import { Socket } from "../types/socket.io";
-import CryptoJS from "crypto-js"
+import { createHash } from "node:crypto";
 
-/**
- * Generates a userID.
- * @param socket The instance of socket.
- * @returns The generated ID
- */
-export default async function magic(socket: Socket): Promise<string> {
-    if (!process.env.PASSWORD) throw new Error("No password provided")
-    const cipher = CryptoJS.AES.encrypt(socket.conn.remoteAddress, process.env.PASSWORD)
-    return cipher.toString()
-}
-export async function decrypt(cipher: CryptoJS.lib.CipherParams){
-    if (!process.env.PASSWORD) throw new Error("No password provided")
-    return CryptoJS.AES.decrypt(cipher, process.env.PASSWORD).toString(CryptoJS.enc.Utf8)
+export default function generateID(ip: string): string {
+    return createHash("sha256").update(ip).digest("hex").toUpperCase();
 }
