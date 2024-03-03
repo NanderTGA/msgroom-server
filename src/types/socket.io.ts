c0b70135f7d2dc476b000dfd55a736e40045116f
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2024-03-03 12:38:32 +0100

    fix: outdated types

--- src/types/socket.io.ts
+++ src/types/socket.io.ts
@@ -1,18 +1,19 @@
-import { ClientToServerEvents, ServerToClientEvents as OfficialServerToClientEvents } from "msgroom/dist/types/socket.io";
-export { ClientToServerEvents, OfficialServerToClientEvents as ServerToClientEvents };
-import { Socket as ServerSideSocket } from "socket.io";
+import type {
+    ClientToServerEvents as OfficialClientToServerEvents,
+    ServerToClientEvents as OfficialServerToClientEvents,
+} from "msgroom/types/socket.io";
+import { Server as SocketIOServer } from "socket.io";
 
-export interface ServerToClientEvents extends OfficialServerToClientEvents {
-    "auth-complete": (userID: string, sessionID?: string) => void;
-}
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface ClientToServerEvents extends OfficialClientToServerEvents {}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface ServerToClientEvents extends OfficialServerToClientEvents {}
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface InterServerEvents {}
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 export interface SocketData {
     ID: string
     session_id: string
 }
-
-export type Socket = ServerSideSocket<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>;
