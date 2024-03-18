d30a9ba4518a5f145e75c300e585922ed5e47a84
NanderTGA <65074195+NanderTGA@users.noreply.github.com>
2024-03-18 22:41:13 +0100

    feat: add update script

---
+++ update.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+# This script is ran daily at 18:00 (6:00PM) in my timezone
+cd "$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+nvs use lts
+pm2 stop msgroom-server --watch
+git reset --hard
+git pull
+npm i
+npm audit fix
+npm run build
+pm2 start msgroom-server
